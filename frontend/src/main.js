d46e02aa1811c83416679499789e93b815b654b4
Andrea Gonzato <gonzatoandrea98@gmail.com>
2023-01-09 11:20:00 +0100

    frontend: add router dir

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,18 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '/views/Home.vue'
-import About from '/views/About.vue'
 
-const router = createRouter({
-    history : createWebHistory(),
-    routes : [
-        {path : "/" , name: 'Home', component: Home},
-        {path : "/about" , name: 'About', component: About}
-    ]
-
-})
+import router from './router'
 
 
 createApp(App)
