2d68f96500e2cf3ace48b73505044e9fb53d5073
Andrea Gonzato <gonzatoandrea98@gmail.com>
2023-01-09 11:26:20 +0100

    define @ as alias for src

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,8 @@
 
 import { createRouter, createWebHistory } from 'vue-router'
 
-import Home from '/views/Home.vue'
-import About from '/views/About.vue'
+import Home from '@/views/Home.vue'
+import About from '@/views/About.vue'
 
 const routes = [
     {path : "/" , name: 'Home', component: Home},
