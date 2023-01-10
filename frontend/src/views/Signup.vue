7ed1345b7ba9d68fa245d838f8f24c8b1867d07b
Andrea Gonzato <gonzatoandrea98@gmail.com>
2023-01-10 11:21:58 +0100

    FE: signup form pt1

--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -3,6 +3,38 @@
     <h1>Signup</h1>
 
     <form>
+      <div class="row">
+        <div class="col">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="First name"
+            aria-label="First name"
+            v-model="name"
+          />
+        </div>
+        <div class="col">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Last name"
+            aria-label="Last name"
+            v-model="surname"
+          />
+        </div>
+      </div>
+
+      <div class="mb-3">
+        <label for="usernameID" class="form-label">username</label>
+        <input
+          type="text"
+          v-model="username"
+          id="usernameID"
+          name="email"
+          class="form-control"
+        />
+      </div>
+
       <div class="mb-3">
         <label for="emailID" class="form-label">email</label>
         <input
@@ -26,30 +58,43 @@
       </div>
 
       <button @click.prevent="login" class="btn btn-primary">Sign up</button>
-
-
     </form>
+
+    <p>
+      name: {{ this.name }} surname: {{ this.surname }} username:
+      {{ this.username }} email: {{ this.email }} password: {{ this.password }}
+    </p>
   </div>
 </template>
 
 <script>
 export default {
   name: "Signup",
-  data(){
+  data() {
     return {
-        email : '',
-        password : '',
-        username : '',
-        name : '',
-        surname : '',
-        bio : "Hi! I'm on Express",
-    }
-  }
+      email: "",
+      password: "",
+      username: "",
+      name: "",
+      surname: "",
+      bio: "Hi! I'm on Express",
+    };
+  },
 };
 </script>
 
 <style scoped>
 .signup {
   text-align: center;
+}
+
+.mb-3 {
+  max-width: 350px;
+  margin: auto;
+}
+
+.row{
+    max-width: 350px;
+    margin: auto;
 }
 </style>
