1b8d0a85007d2da7c7930407575636f3c45e20b1
Andrea Gonzato <gonzatoandrea98@gmail.com>
2023-01-14 10:07:10 +0100

    add some html to Account page

--- frontend/src/views/Account.vue
+++ frontend/src/views/Account.vue
@@ -1,11 +1,48 @@
 <template>
-    <div class="all">
-        <h1>Account</h1>
+  <div class="all">
+    <h1>Account</h1>
+
+    <div class="container text-center info">
+      <div class="row align-items-start">
+        <div class="col data-type">Username:</div>
+        <div class="col data">{{ this.username }}</div>
+      </div>
+      <div class="row align-items-start">
+        <div class="col data-type">Email:</div>
+        <div class="col data">{{ this.email }}</div>
+      </div>
     </div>
+
+    <button @click.prevent="logout" class="btn btn-danger">Log out</button>
+  </div>
 </template>
 
+<script>
+export default {
+  name: "Account",
+  data() {
+    return {
+      username: "",
+      email: "",
+    };
+  },
+};
+</script>
+
 <style scoped>
-.all{
-    text-align: center;
+.all {
+  text-align: center;
+}
+
+.info{
+    max-width: 400px;
+    margin-top: 5em;
+    margin-bottom: 5em;
+}
+.data-type{
+    text-align: right;
+}
+.data{
+    text-align: left;
 }
 </style>
