f3bcca0562af475d494c3c0d4a06b51f06134845
Andrea Gonzato <gonzatoandrea98@gmail.com>
2023-01-18 14:38:50 +0100

    use of UserInfo in the myAccount and Account page

--- frontend/src/components/UserInfo.vue
+++ frontend/src/components/UserInfo.vue
@@ -8,12 +8,12 @@
 
       <div class="row align-items-start">
         <div class="col data-type">Name:</div>
-        <div class="col data">{{ userObj.name }}</div>
+        <div class="col data">{{ this.getName }}</div>
       </div>
 
       <div class="row align-items-start">
         <div class="col data-type">Surname:</div>
-        <div class="col data">{{ userObj.surname }}</div>
+        <div class="col data">{{ this.getSurname }}</div>
       </div>
 
       <div class="row align-items-start">
@@ -35,13 +35,21 @@ export default {
   props: {
     userObj: Object,
   },
+  computed: {
+    getName() {
+      return this.userObj.name.charAt(0).toUpperCase() + this.userObj.name.substring(1);
+    },
+    getSurname() {
+      return this.userObj.surname.charAt(0).toUpperCase() + this.userObj.surname.substring(1);
+    },
+  },
 };
 </script>
 
 
 <style scoped>
 .info {
-  max-width: 400px;
+  max-width: 500px;
   margin-top: 5em;
   margin-bottom: 5em;
 }
--- frontend/src/views/MyAccount.vue
+++ frontend/src/views/MyAccount.vue
@@ -2,31 +2,7 @@
   <div class="all">
     <h1>Account</h1>
 
-
     <UserInfo v-if="loadedUser" v-bind:userObj="this.user"></UserInfo>
-
-    <div class="container text-center info">
-      <div class="row align-items-start">
-        <div class="col data-type">Name:</div>
-        <div class="col data">{{ getName }}</div>
-      </div>
-      <div class="row align-items-start">
-        <div class="col data-type">Surname:</div>
-        <div class="col data">{{ getSurname }}</div>
-      </div>
-      <div class="row align-items-start">
-        <div class="col data-type">Username:</div>
-        <div class="col data">{{ this.username }}</div>
-      </div>
-      <div class="row align-items-start">
-        <div class="col data-type">Email:</div>
-        <div class="col data">{{ this.email }}</div>
-      </div>
-      <div class="row align-items-start">
-        <div class="col data-type">Bio:</div>
-        <div class="col data">{{ this.bio }}</div>
-      </div>
-    </div>
 
     <button @click.prevent="logout" class="btn btn-danger">Log out</button>
   </div>
@@ -41,11 +17,8 @@ export default {
   name: "MyAccount",
   data() {
     return {
-      name: "",
-      surname: "",
-      username: "",
-      email: "",
-      bio: "",
+      user : Object,
+      loadedUser : false
     };
   },
   components: {
@@ -55,20 +28,9 @@ export default {
     // tell the app to show account in the nav menu and remove login and signin
     this.$emit("message", { userLogged: true });
     
-    const user = await userManager.whoami();
-    this.name = user.name;
-    this.surname = user.surname;
-    this.username = user.username;
-    this.email = user.email;
-    this.bio = user.bio;
-  },
-  computed: {
-    getName() {
-      return this.name.charAt(0).toUpperCase() + this.name.substring(1);
-    },
-    getSurname() {
-      return this.surname.charAt(0).toUpperCase() + this.surname.substring(1);
-    },
+    this.user = await userManager.whoami();
+    this.loadedUser = true;
+  
   },
   methods: {
     logout() {
@@ -85,16 +47,4 @@ export default {
   text-align: center;
 }
 
-.info {
-  max-width: 800px;
-  margin-top: 5em;
-  margin-bottom: 5em;
-  display: grid;
-}
-.data-type {
-  text-align: right;
-}
-.data {
-  text-align: left;
-}
 </style>
