70f88d0abc648fa01e52737e04d9b45f5a5e9018
Andrea Gonzato <gonzatoandrea98@gmail.com>
2023-01-19 10:20:42 +0100

    MyAccount page def settings and your express

--- frontend/src/views/MyAccount.vue
+++ frontend/src/views/MyAccount.vue
@@ -2,9 +2,22 @@
   <div class="all">
     <h1>Account</h1>
 
-    <UserInfo v-if="loadedUser" v-bind:userObj="this.user"></UserInfo>
+    <div class="subMenu">
+      <button @click.prevent="loadYourMessages" class="btn btn-secondary">
+        Your Express
+      </button>
+      <button @click.prevent="loadSettings" class="btn btn-secondary">
+        Settings
+      </button>
+    </div>
 
-    <button @click.prevent="logout" class="btn btn-danger">Log out</button>
+    <div class="settings" v-if="this.showSettings">
+      <UserInfo v-if="loadedUser" v-bind:userObj="this.user"></UserInfo>
+
+<button @click.prevent="logout" class="btn btn-danger">Log out</button>
+    </div>
+
+
   </div>
 </template>
 
@@ -17,20 +30,20 @@ export default {
   name: "MyAccount",
   data() {
     return {
-      user : Object,
-      loadedUser : false
+      user: Object,
+      loadedUser: false,
+      showSettings: false
     };
   },
   components: {
-    UserInfo
+    UserInfo,
   },
   async created() {
     // tell the app to show account in the nav menu and remove login and signin
     this.$emit("message", { userLogged: true });
-    
+
     this.user = await userManager.whoami();
     this.loadedUser = true;
-  
   },
   methods: {
     logout() {
@@ -38,6 +51,12 @@ export default {
       cookieManager.removeJwtCookie();
       this.$router.push({ name: "Home" });
     },
+    loadSettings(){
+      this.showSettings = true;
+    },
+    loadYourMessages(){
+      this.showSettings = false;
+    }
   },
 };
 </script>
@@ -47,4 +66,11 @@ export default {
   text-align: center;
 }
 
+.subMenu{
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  max-width: 400px;
+  margin: auto;
+  grid-gap: 3em;
+}
 </style>
