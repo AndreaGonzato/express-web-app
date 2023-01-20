ed0a80ed2b248d2a896eb2f76c8cc0d1563e3bc0
Andrea Gonzato <gonzatoandrea98@gmail.com>
2023-01-20 09:46:45 +0100

    add table to the result of users search

--- frontend/src/components/UserSearchResult.vue
+++ frontend/src/components/UserSearchResult.vue
@@ -1,15 +1,35 @@
 <template>
-    <div class="all">
-        {{this.userObj.username}}
+  <div class="all">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm first-col">
+          <router-link v-bind:to="'/account/' + this.userObj.username"
+            >@{{ this.userObj.username }}</router-link
+          >
+        </div>
+        <div class="col-sm">{{ this.userObj.name }}</div>
+        <div class="col-sm">{{ this.userObj.surname }}</div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-
-export default{
-    name: "UserSearchResult",
-    props: {
-        userObj : Object
-    }
-}
+export default {
+  name: "UserSearchResult",
+  props: {
+    userObj: Object,
+  },
+};
 </script>
+
+<style scoped>
+.container{
+    max-width: 500px;
+    text-align: left;
+}
+
+.first-col{
+    text-align: left;
+}
+</style>
