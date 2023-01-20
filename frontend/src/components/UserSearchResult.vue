1dd7406228c24dc9cfe4fdfa6dad23dda24ceb7f
Andrea Gonzato <gonzatoandrea98@gmail.com>
2023-01-20 10:13:51 +0100

    full name an follow button add

--- frontend/src/components/UserSearchResult.vue
+++ frontend/src/components/UserSearchResult.vue
@@ -7,8 +7,10 @@
             >@{{ this.userObj.username }}</router-link
           >
         </div>
-        <div class="col-sm">{{ this.userObj.name }}</div>
-        <div class="col-sm">{{ this.userObj.surname }}</div>
+        <div class="col-sm">{{ this.fulName }}</div>
+        <div class="col-sm">
+            <button @click.prevent="follow" class="btn btn-primary">Follow</button>
+        </div>
       </div>
     </div>
   </div>
@@ -20,10 +22,30 @@ export default {
   props: {
     userObj: Object,
   },
+  computed:{
+    getName() {
+      return this.userObj.name.charAt(0).toUpperCase() + this.userObj.name.substring(1);
+    },
+    getSurname() {
+      return this.userObj.surname.charAt(0).toUpperCase() + this.userObj.surname.substring(1);
+    },
+    fulName(){
+        return this.getName + " " + this.getSurname;
+    }
+  },
+  methods: {
+    follow(){
+        console.log("To implement", this.userObj.name)
+    }
+  }
 };
 </script>
 
 <style scoped>
+button{
+    margin-top: 0.2em;
+    margin-bottom: 0.2em;
+}
 .container{
     max-width: 500px;
     text-align: left;
